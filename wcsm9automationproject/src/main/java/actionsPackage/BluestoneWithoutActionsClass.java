7c19fa6f64157cdbd9ffdbc65b80f0b8d52e4061
Admin <Admin@DESKTOP-LMCETQJ>
2023-10-25 12:52:02 -0700

    second commit

---
+++ wcsm9automationproject/src/main/java/actionsPackage/BluestoneWithoutActionsClass.java
@@ -0,0 +1,40 @@
+package actionsPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class BluestoneWithoutActionsClass {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://www.bluestone.com/");
+		//handle popup
+		driver.findElement(By.id("denyBtn")).click();
+		
+	    WebElement coins = driver.findElement(By.xpath("//a[text()='Coins ']"));
+	    Actions act = new Actions(driver);
+	    act.moveToElement(coins).perform();
+	    
+	    driver.findElement(By.xpath("//span[@text()='Coins by Design']")).click();
+	    if(driver.findElement(By.xpath("//h1[text()='Lakshmigoldcoins']")).isDisplayed())
+	    {
+	    	System.out.println("text displayed!!");
+	    }
+	    else
+	    {
+	    	System.out.println("text is not displayed!!");
+	    }
+	    
+		
+	}
+	
+	
+}
+
+
